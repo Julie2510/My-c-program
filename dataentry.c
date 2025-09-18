b4d82befae07fe9a713df946312aa7d272015944
Julie2510 <julianamuiruri966@gmail.com>
2025-09-18 08:54:02 +0300

    Add files via upload

---
+++ dataentry.c
@@ -0,0 +1,30 @@
+//simple C program 
+/*
+Name: Juliana 
+Reg:CT100/G/26246/25
+Description: data entry 
+*/
+
+#include <stdio.h>
+
+//main function 
+int main() 
+{
+   int height;
+   int contact;
+   int weight;
+
+  printf("enter your height:");
+  scanf("%d", & height);
+  printf("your height is %d \n");
+
+  printf("enter your contact:");
+  scanf ("%d",& contact);
+  printf("your contact is %d \n");
+
+  printf("enter your weight:");
+  scanf ("%d", & weight);
+  printf("your weight is %d \n");
+
+return 0;
+}
---
+++ semesterunits.c
@@ -0,0 +1,21 @@
+
+//simple C program (units)
+/*
+Name:Juliana 
+Reg:CT100/G/26246/25
+Description:Sem Units
+*/
+
+#include <stdio.h>
+int main() {
+  printf("Semester Units \n");
+  printf("Analogue electronics \n");
+  printf("Introduction to programming and algorithm \n");
+  printf("Computer system and organization \n");
+  printf("Introduction to computer application \n");
+  printf("Essentials of accounting \n");
+  printf("Mathematics for science \n");
+  printf("Fundamentals of ICT \n");
+  printf("Communication and information literature \n");
+return 0;
+}
